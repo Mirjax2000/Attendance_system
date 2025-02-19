633d5e9694d470bb91959922ad448592571d749a
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-19 14:56:15 +0100

    path to img

--- app_main/models.py
+++ app_main/models.py
@@ -91,7 +91,7 @@ class Employee(Model):
 class UserPicture(Model):
     """Users picture table"""
 
-    PATH_TO_IMG: str = "employee_img/"
+    PATH_TO_IMG = str(settings.MEDIA_ROOT)
     # cesta k obrazkum pro DB
 
     employee = OneToOneField(
