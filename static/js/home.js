2152d7772195ad48339c864597ddf78e4c49e81e
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-22 17:43:54 +0100

    makame jedeme

---
+++ static/js/home.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+    const employeeLink = $("#employee");
+    const employeeSublist = $("#employeeSublist");
+
+    employeeLink.on("click", function (event) {
+        event.preventDefault();
+        this.dataset.pointer = this.dataset.pointer === "left" ? "down" : "left";
+        this.dataset.show = this.dataset.pointer === "down" ? "true" : "false";
+
+        employeeSublist.slideToggle(500);
+    });
+});
