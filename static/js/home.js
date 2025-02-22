15a642077079680ef5e9294323b16d7f862a8e6e
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-22 19:55:42 +0100

    makame jedeme

--- static/js/home.js
+++ static/js/home.js
@@ -3,6 +3,10 @@ $(function () {
 
     const employeeLink = $("#employee");
     const employeeSublist = $("#employeeSublist");
+    const content = $("#content");
+    const navMenu = $(".l-nav");
+    const navlinks = navMenu.find('a')
+
 
     employeeLink.on("click", function (event) {
         event.preventDefault();
@@ -10,6 +14,22 @@ $(function () {
         this.dataset.show = this.dataset.pointer === "down" ? "true" : "false";
 
         employeeSublist.slideToggle(500);
+    });
+
+    navlinks.on('click', function (e) {
+        e.preventDefault();
+        const url = $(this).data('url');
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function (html) {
+                content.html(html);
+            },
+            error: function (xhr, status, error) {
+                console.error('Chyba:', error);
+            }
+        });
     });
 })
 
