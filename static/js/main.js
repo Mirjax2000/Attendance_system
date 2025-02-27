6fd30c8444f42aec58e29ca04c3be24a6335a12e
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-27 19:22:54 +0100

    casova funkce

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,21 @@
 (() => {
     console.log('welcome');
     console.log('main.js');
+    //
+    let timeText = document.getElementById('timeText');
+    // casova funkce
+    function updateTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+
+        timeText.textContent = timeString;
+    }
+
+    setInterval(updateTime, 1000);
+
+    updateTime();
 
 })();
