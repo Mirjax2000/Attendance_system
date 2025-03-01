84a6d3c50c665ee7ce490a995f2809beb02f3cae
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-01 22:58:24 +0100

    makame jedeme kodime

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,26 @@
+from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
-from django.contrib import messages
-
 
 
 def user_logout(request):
     """Logout a redirect"""
     logout(request)
-    return redirect('dashboard')
+    return redirect("dashboard")
 
 
 class CustomLoginView(LoginView):
+    """login form"""
+
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('dashboard')
+            return redirect("dashboard")
         return super().get(request, *args, **kwargs)
 
     def form_invalid(self, form):
-        messages.error(self.request,
-                       "Nepodařilo se přihlásit, zkuste to znovu.")
+        messages.error(
+            self.request, "Nepodařilo se přihlásit, zkuste to znovu."
+        )
 
-        return redirect('dashboard')
-
+        return redirect("dashboard")
