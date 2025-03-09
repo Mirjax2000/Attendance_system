1861496f961544d29d0e4b6bc188cd2467c186db
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-09 21:19:10 +0100

    jdem na views a c predavani contextu

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -3,35 +3,11 @@
     console.log('dashboard.js');
     // by ID
     const employeesBtn = document.getElementById("employeesBtn");
-    const navMenu = document.getElementById("navigationPanel");
     const employeeSublistJQ = $("#employeesSublist"); /* pro JQ slide */
 
-    // Nodelists
-    const navLinks = navMenu.querySelectorAll(".fetch-url");
-    // Arrays
-    const sublistLinksArray = Array.from(navMenu.querySelectorAll(".sublink"));
-    // settings
     const animationTime = 200;
 
-    // prace s fetchlinkama - jako Nodelist
-    fetchLinks.forEach(function (element) {
-        // hlavni nacitaci stranka
 
-        element.addEventListener('click', function (e) {
-            e.preventDefault()
-            // remove active_link Class
-            fetchLinks.forEach(function (link) {
-                link.classList.remove("active_link")
-            })
-            // pridej classu activ_link na link
-            this.classList.add("active_link")
-
-            if (this !== employeesBtn && !sublistLinksArray.includes(this)) {
-                employeeSublistJQ.stop(true, true).slideUp(animationTime);
-                employeesBtn.dataset.pointer = "left";
-            }
-        })
-    })
     // rozbaleni sublistu employeesBtn
     employeesBtn.addEventListener("click", function (e) {
         e.preventDefault()
