341ddaf47b327d7228740cf3271745cec27988e6
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-09 18:39:25 +0100

    zatim dobry

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -12,25 +12,10 @@
     const sublistLinksArray = Array.from(navMenu.querySelectorAll(".sublink"));
     // settings
     const animationTime = 200;
-    // 
-    // --- Fetch function pro innerHTML ---
-    async function loadContent(url, HTMLelement) {
-        try {
-            let response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Chyba ${response.status}: ${response.statusText}`);
-            }
-            HTMLelement.innerHTML = await response.text();
-        } catch (error) {
-            console.error("Chyba:", error);
-        }
-    }
+
     // prace s fetchlinkama - jako Nodelist
     fetchLinks.forEach(function (element) {
         // hlavni nacitaci stranka
-        if (element.classList.contains("active_link")) {
-            loadContent(element.dataset.url, content);
-        }
 
         element.addEventListener('click', function (e) {
             e.preventDefault()
@@ -40,8 +25,6 @@
             })
             // pridej classu activ_link na link
             this.classList.add("active_link")
-            // spust fetch funkci s dataset url parametrem
-            loadContent(this.dataset.url, content);
 
             if (this !== employeesBtn && !sublistLinksArray.includes(this)) {
                 employeeSublistJQ.stop(true, true).slideUp(animationTime);
