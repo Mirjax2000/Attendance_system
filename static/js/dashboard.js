caeb27b3c41a7b6c59f72835ea2d676d490f57f5
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-13 15:02:50 +0100

    prechod na verzi .8

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -2,11 +2,12 @@
 // IIFE - izolace promennych GlobeScope pouze v teto noname funkci
 $(function () {
     console.log('dashboard.js');
-    let filterContainerJQ = $(".c-filter");
-    filterContainerJQ.children(".c-filter__container").hide(1)
+    let filterJQ = $(".c-filter");
 
-    filterContainerJQ.on("click", function (e) {
-        $($(this).children(".c-filter__container")).stop(true, true).slideToggle();
+    filterJQ.children(".c-filter__container").hide(1)
+
+    filterJQ.children(".c-filter__banner").on("click", function (e) {
+        $($(this).next(".c-filter__container")).stop(true, true).slideToggle();
     });
 
 
