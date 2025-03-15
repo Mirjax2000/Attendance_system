0d2c97f2ffa5c7c6bbc281dc09d27117d4f6d97f
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-15 16:05:02 +0100

    nov model employee status

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -11,33 +11,32 @@ $(function () {
     });
 
 
-    // document.getElementById("myForm").addEventListener("submit", async function (e) {
-    //     e.preventDefault();
+    document.getElementById("myForm").addEventListener("submit", async function (e) {
+        e.preventDefault();
 
-    //     const form = e.target;
-    //     const formData = new FormData(form);
-    //     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+        const form = e.target;
+        const formData = new FormData(form);
+        const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-    //     // Odeslání požadavku pomocí fetch
-    //     try {
-    //         const response = await fetch("/dashboard/save_vector_to_db", {
-    //             method: "POST",
-    //             headers: {
-    //                 "X-CSRFToken": csrftoken,  // Ochrana proti CSRF
-    //             },
-    //             body: formData,  
-    //         });
+        try {
+            const response = await fetch("/dashboard/save_vector_to_db", {
+                method: "POST",
+                headers: {
+                    "X-CSRFToken": csrftoken,  // Ochrana proti CSRF
+                },
+                body: formData,  
+            });
 
-    //         if (!response.ok) {
-    //             throw new Error("Chyba při odesílání dat");
-    //         }
+            if (!response.ok) {
+                throw new Error("Chyba při odesílání dat");
+            }
 
-    //         const data = await response.json();
+            const data = await response.json();
 
-    //     } catch (error) {
-    //         console.error("Error:", error);
-    //     }
-    // });
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
 
 
 });
