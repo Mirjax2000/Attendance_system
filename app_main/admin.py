1ee5fe0268f5bde64136382b153e8163290f6971
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-15 16:05:36 +0100

    nastaven adminu

--- app_main/admin.py
+++ app_main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from app_main.models import Department, Employee, FaceVector
+from app_main.models import Department, Employee, EmployeeStatus, FaceVector
 
 admin.site.register(Employee)
 admin.site.register(FaceVector)
 admin.site.register(Department)
+admin.site.register(EmployeeStatus)
