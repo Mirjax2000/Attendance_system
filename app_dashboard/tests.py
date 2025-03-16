da333c1f979d04956d585058b62be7c05bdd50ee
DaliborZ-tech <dalibor.zalesak@gmail.com>
2025-03-16 18:28:20 +0100

    Added EmployeeForm tests
    Introduced a new test suite for EmployeeForm to ensure proper validation, including required fields, duplicate emails, and input formatting. These tests cover key validation scenarios like postal code, pin code, and email uniqueness. This enhances the reliability and correctness of the EmployeeForm validation logic.

--- app_dashboard/tests.py
+++ app_dashboard/tests.py
@@ -6,7 +6,8 @@ from datetime import date
 import numpy as np
 from django.test import TestCase
 
-from app_main.models import Employee, FaceVector
+from app_dashboard.forms import EmployeeForm
+from app_main.models import Employee, FaceVector, Department, EmployeeStatus
 
 seed: int = 42
 rng = np.random.default_rng(seed)
@@ -65,7 +66,9 @@ class EmployeeModelTest(TestCase):
         self.assertEqual(employee_jan.postal_code, "10000")
         self.assertEqual(employee_jan.phone_number, "+420123456789")
         self.assertEqual(employee_jan.email, "jan.novak@example.com")
-        self.assertEqual(employee_jan.date_of_birth, date(1985, 5, 10))
+        self.assertEqual(employee_jan.date_of_birth, date(
+            1985, 5, 10)
+                         )
         self.assertEqual(employee_jan.age(), (39))
         self.assertTrue(employee_jan.check_pin_code("1234"))
         self.assertEqual(employee_jan.is_valid, True)
@@ -89,3 +92,105 @@ class EmployeeModelTest(TestCase):
             expected_vector_2,
         )
         self.assertFalse(face_vector_jan.face_vector)
+
+
+class EmployeeFormTests(TestCase):
+    def setUp(self):
+        """Vytvoření potřebných objektů.""" 
+        self.department = Department.objects.create(name="IT Department")
+        self.employee_status = EmployeeStatus.objects.create(name="Active")
+
+        self.existing_employee = Employee.objects.create(
+            name="Josef",
+            surname="Drtikol",
+            street_number="Vrsovicka 12",
+            city="Praha",
+            postal_code="11000",
+            phone_number="123456789",
+            email="testuser@seznam.cz",
+            date_of_birth="1989-02-13",
+            pin_code="1234",
+            department=self.department,
+            employee_status=self.employee_status
+        )
+
+    def get_valid_form_data(self):
+        """Pomocná metoda vracející validní data formuláře."""
+        return {
+            "name": "Karel",
+            "surname": "Tanker",
+            "street_number": "Zelnak 456",
+            "city": "Brno",
+            "postal_code": "60200",
+            "phone_number": "987654321",
+            "email": "karel.tanker@tankovna.cz",
+            "date_of_birth": "1975-05-15",
+            "pin_code": "4321",
+            "department": self.department.pk,
+            "employee_status": self.employee_status.pk,
+        }
+
+    def test_valid_form(self):
+        """Formulář s platnými daty by měl projít validací."""
+        form_data = self.get_valid_form_data()
+        form = EmployeeForm(data=form_data)
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_required_field_name(self):
+        """Testuje, že pole 'name' je povinné."""
+        form_data = self.get_valid_form_data()
+        form_data["name"] = ""
+        form = EmployeeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("name", form.errors)
+        self.assertIn("Jméno je povinné!", form.errors["name"])
+
+    def test_required_field_surname(self):
+        """Testuje, že pole 'surname' je povinné."""
+        form_data = self.get_valid_form_data()
+        form_data["surname"] = ""
+        form = EmployeeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("surname", form.errors)
+        self.assertIn("Příjmení je povinné!", form.errors["surname"])
+
+    def test_duplicate_email(self):
+        """
+        Testuje, že při zadání e-mailu, který již existuje 
+        (upraví i velikost písma),dojde k chybě.
+        """
+        form_data = self.get_valid_form_data()
+        form_data["email"] = "TESTUSER@seznam.cz"
+        form = EmployeeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors)
+        self.assertEqual(
+            form.errors["email"],
+            ["Tento e-mail je již používán. Zvolte jiný."]
+        )
+
+    def test_invalid_postal_code(self):
+        """
+        Testuje, že je vyvolána chyba validace pro PSČ, 
+        pokud PSČ neobsahuje přesně 5 číslic.
+        """
+        form_data = self.get_valid_form_data()
+        form_data["postal_code"] = "1234"
+        form = EmployeeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("postal_code", form.errors)
+        self.assertIn("PSČ musí obsahovat 5 číslic.",
+                      form.errors["postal_code"])
+
+    def test_invalid_pin_code(self):
+        """
+        Testuje, že je vyvolána chyba validace, 
+        pokud PIN kód neobsahuje přesně 4 číslice.
+        """
+        form_data = self.get_valid_form_data()
+        form_data["pin_code"] = "123"
+        form = EmployeeForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("pin_code", form.errors)
+        self.assertIn("PIN kód musí obsahovat přesně 4 číslice.",
+                      form.errors["pin_code"])
