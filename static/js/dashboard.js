d70aa59a1a2989bcffa80745859fadc951dbd560
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-20 12:40:25 +0100

    js ready color-dataset

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -3,6 +3,9 @@
 $(function () {
     console.log('dashboard.js');
     const filterJQ = $(".c-filter");
+    const cardList = document.querySelectorAll(".L-departments__list");
+
+
 
 
     filterJQ.children(".c-filter__container").hide(1)
@@ -11,6 +14,18 @@ $(function () {
         $($(this).next(".c-filter__container")).stop(true, true).slideToggle();
     });
 
+    // zmena datasetu na barvy v c-count cards v L-department layoutu
+    cardList.forEach(function (element) {
+        let num = 1
+        let cards = element.querySelectorAll(".c-count__card__content")
 
+        cards.forEach(function (card) {
+            card.dataset.color = `clr_${num}`
+            num += 1
+            if (num > 6) {
+                num = 1
+            }
+        })
+    })
 
 });
