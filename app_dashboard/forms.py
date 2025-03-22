15aea81d67a64f7511f28144bc01914a62f5cdef
DaliborZ-tech <dalibor.zalesak@gmail.com>
2025-03-22 14:49:00 +0100

    Make form field validation methods handle None values

--- app_dashboard/forms.py
+++ app_dashboard/forms.py
@@ -59,66 +59,80 @@ class EmployeeForm(forms.ModelForm):
             "pin_code": forms.PasswordInput(attrs={"maxlength": 4}),
         }
 
-    def clean_email(self) -> str:
+    def clean_email(self) -> str or None:
         """Kontrola emailu a převod na lowercase"""
-        email: str = self.cleaned_data.get("email").lower()
-        existing_emails = Employee.objects.filter(email__iexact=email)
-        if self.instance.pk:
-            existing_emails = existing_emails.exclude(pk=self.instance.pk)
-        if existing_emails.exists():
-            raise ValidationError("Tento e-mail je již používán. Zvolte jiný.")
-        return email
+        if self.cleaned_data.get("email") is not None:
+            email: str = self.cleaned_data.get("email").lower()
+            existing_emails = Employee.objects.filter(email__iexact=email)
+            if self.instance.pk:
+                existing_emails = existing_emails.exclude(pk=self.instance.pk)
+            if existing_emails.exists():
+                raise ValidationError("Tento e-mail je již používán. Zvolte jiný.")
+            return email
+        return None
 
-    def clean_postal_code(self) -> str:
+    def clean_postal_code(self) -> str or None:
         """Kontrola formátu a délky psč, odstranění mezer"""
-        postal_code: str = self.cleaned_data.get('postal_code', '').strip()
-        postal_code_no_spaces = postal_code.replace(' ', '')
-        if (len(postal_code_no_spaces
-                ) != 5 or not postal_code_no_spaces.isdigit()):
-            raise ValidationError(
-                "PSČ musí obsahovat přesně 5 číslic bez mezer.")
-        return postal_code_no_spaces
+        if self.cleaned_data.get("postal_code") is not None:
+            postal_code: str = self.cleaned_data.get('postal_code', '').strip()
+            postal_code_no_spaces = postal_code.replace(' ', '')
+            if (len(postal_code_no_spaces
+                    ) != 5 or not postal_code_no_spaces.isdigit()):
+                raise ValidationError(
+                    "PSČ musí obsahovat přesně 5 číslic bez mezer.")
+            return postal_code_no_spaces
+        return None
 
-    def clean_phone_number(self) -> str:
+    def clean_phone_number(self) -> str or None:
         """Kontrola formátu tel. čísla"""
-        phone_number: str = self.cleaned_data.get('phone_number')
-        pattern: str = r'^\+420\s?(\d{3}\s?\d{3}\s?\d{3})$|^\d{9}$'
-        if not phone_number:
-            raise ValidationError("Telefonní číslo je povinné.")
-        if not re.match(pattern, phone_number):
-            raise ValidationError(
-                "Telefonní číslo musí být ve formátu +420 XXX XXX XXX nebo "
-                "9 číslic bez mezer."
-            )
-        return phone_number
+        if self.cleaned_data.get("phone_number") is not None:
+            phone_number: str = self.cleaned_data.get('phone_number')
+            pattern: str = r'^\+420\s?(\d{3}\s?\d{3}\s?\d{3})$|^\d{9}$'
+            if not phone_number:
+                raise ValidationError("Telefonní číslo je povinné.")
+            if not re.match(pattern, phone_number):
+                raise ValidationError(
+                    "Telefonní číslo musí být ve formátu +420 XXX XXX XXX nebo "
+                    "9 číslic bez mezer."
+                )
+            return phone_number
+        return None
 
-    def clean_pin_code(self) -> str:
+    def clean_pin_code(self) -> str or None:
         """Kontrola formátu pin kódu"""
-        pin_code: str = self.cleaned_data.get('pin_code')
-        if not pin_code.isdigit() or len(pin_code) != 4:
-            raise ValidationError("PIN kód musí obsahovat přesně 4 číslice.")
-        return pin_code
+        if self.cleaned_data.get("pin_code") is not None:
+            pin_code: str = self.cleaned_data.get('pin_code')
+            if not pin_code.isdigit() or len(pin_code) != 4:
+                raise ValidationError("PIN kód musí obsahovat přesně 4 číslice.")
+            return pin_code
+        return None
 
-    def clean_date_of_birth(self) -> date:
+    def clean_date_of_birth(self) -> date or None:
         """Kontrola data narození (nesmí být v budoucnu)"""
-        date_of_birth: date = self.cleaned_data.get('date_of_birth')
-        if not date_of_birth:
-            raise ValidationError("Datum narození je povinné.")
-        if date_of_birth > date.today():
-            raise ValidationError("Datum narození nemůže být v budoucnosti.")
-        return date_of_birth
+        if self.cleaned_data.get("date_of_birth") is not None:
+            date_of_birth: date = self.cleaned_data.get('date_of_birth')
+            if not date_of_birth:
+                raise ValidationError("Datum narození je povinné.")
+            if date_of_birth > date.today():
+                raise ValidationError("Datum narození nemůže být v budoucnosti.")
+            return date_of_birth
+        return None
 
-    def clean_name(self) -> str:
+    def clean_name(self) -> str or None:
         """Kontrola jména (velké písmeno a znaky)"""
-        name: str = self.cleaned_data.get('name').strip().capitalize()
-        if not name.isalpha():
-            raise ValidationError("Jméno smí obsahovat pouze znaky abecedy.")
-        return name
+        if self.cleaned_data.get('name') is not None:
+            name: str = self.cleaned_data.get('name').strip().capitalize()
+            if not name.isalpha():
+                raise ValidationError("Jméno smí obsahovat pouze znaky abecedy.")
+            return name
+        return None
 
-    def clean_surname(self) -> str:
+    def clean_surname(self) -> str or None:
         """Kontrola příjmení (velké písmeno a znaky)"""
-        surname: str = self.cleaned_data.get('surname').strip().capitalize()
-        if not surname.isalpha():
-            raise ValidationError(
-                "Příjmení smí obsahovat pouze znaky abecedy.")
-        return surname
+        if self.cleaned_data.get('surname') is not None:
+            surname: str = self.cleaned_data.get('surname').strip().capitalize()
+            if not surname.isalpha():
+                raise ValidationError(
+                    "Příjmení smí obsahovat pouze znaky abecedy.")
+            return surname
+        return None
