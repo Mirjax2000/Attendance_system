197748d7a9e6434b974b7ad75f929633c62f3725
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-03-23 19:22:07 +0100

    updae history manager

--- attendance/history_status_manager.py
+++ attendance/history_status_manager.py
@@ -1,3 +1,6 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import BaseManager
+
 """OOP history status manager"""
 
 from app_main.models import (
@@ -11,8 +14,9 @@ from app_main.models import (
 class HistoryStatusManager:
     """History status manager"""
 
-    def __init__(self, employee_slug: str):
-        self.employee_slug = employee_slug
+    def __init__(self, employee_slug: str) -> None:
+        self.employee_slug: str = employee_slug
+        self.utility = Utility()
 
     def __str__(self) -> str:
         return f"HistoryStatusManager for employee: {self.employee_slug}"
@@ -23,96 +27,75 @@ class HistoryStatusManager:
     def get_worked_hours_on_day(self, day) -> int:
         """jak dlouho pracoval v urcity den"""
         try:
-            employee = Employee.objects.get(slug=self.employee_slug)
+            employee: Employee = Employee.objects.get(slug=self.employee_slug)
 
-            history = EmployeeStatusHistory.objects.filter(
-                employee=employee, timestamp__date=day
+            history: BaseManager[EmployeeStatusHistory] = (
+                EmployeeStatusHistory.objects.filter(
+                    employee=employee, timestamp__date=day
+                )
             )
+            return self.utility.result_by_hours(history)
 
-            worked_time: int = 0
-
-            for record in history:
-                # Pokud byl zaměstnanec ve stavu "working"
-                if record.new_status.name == "working":
-                    # Získáme předchozí status (může být nemocenská, volno apod.)
-                    previous_record = history.filter(
-                        timestamp__lt=record.timestamp
-                    ).last()
-                    if previous_record:
-                        # Vypočítat čas mezi předchozím záznamem a tímto
-                        worked_time += (
-                            record.timestamp - previous_record.timestamp
-                        ).seconds // 3600  # V hodinách
-
-            return worked_time
-
-        except Employee.DoesNotExist:
+        except ObjectDoesNotExist:
             return 0
 
     def get_worked_hours_in_week(self, start_date, end_date) -> int:
         """jak dlouho pracoval v urcitem obdoby"""
         try:
-            employee = Employee.objects.get(slug=self.employee_slug)
+            employee: Employee = Employee.objects.get(slug=self.employee_slug)
 
-            history = EmployeeStatusHistory.objects.filter(
-                employee=employee,
-                timestamp__gte=start_date,
-                timestamp__lte=end_date,
+            history: BaseManager[EmployeeStatusHistory] = (
+                EmployeeStatusHistory.objects.filter(
+                    employee=employee,
+                    timestamp__gte=start_date,
+                    timestamp__lte=end_date,
+                )
             )
 
-            worked_time = 0
+            return self.utility.result_by_hours(history)
 
-            for record in history:
-                if record.new_status.name == "working":
-                    previous_record = history.filter(
-                        timestamp__lt=record.timestamp
-                    ).last()
-                    if previous_record:
-                        worked_time += (
-                            record.timestamp - previous_record.timestamp
-                        ).seconds // 3600  # V hodinách
-
-            return worked_time
-
-        except Employee.DoesNotExist:
+        except ObjectDoesNotExist:
             return 0
 
     def get_time_in_status(self, status_name, start_date, end_date) -> int:
         """jak dlouho byl ve statusu"""
         try:
-            employee = Employee.objects.get(slug=self.employee_slug)
+            employee: Employee = Employee.objects.get(slug=self.employee_slug)
 
             # Získání historie pro daný status a dané časové období
-            history = EmployeeStatusHistory.objects.filter(
-                employee=employee,
-                new_status__name=status_name,
-                timestamp__gte=start_date,
-                timestamp__lte=end_date,
+            history: BaseManager[EmployeeStatusHistory] = (
+                EmployeeStatusHistory.objects.filter(
+                    employee=employee,
+                    new_status__name=status_name,
+                    timestamp__gte=start_date,
+                    timestamp__lte=end_date,
+                )
             )
 
-            time_spent = 0
+            return self.utility.result_by_hours(history)
 
-
-    class Utility:
-
-        @staticmethod
-        def result_by_hours(history)->int:
-            """vysledek preveden na hodiny"""
-            time_spent = 0
-            for record in history:
-                previous_record = history.filter(
-                    timestamp__lt=record.timestamp
-                ).last()
-                if previous_record:
-                    time_spent += (
-                        record.timestamp - previous_record.timestamp
-                    ).seconds // 3600  # V hodinách
-
-            return time_spent
-
-        except Employee.DoesNotExist:
+        except ObjectDoesNotExist:
             return 0
 
 
+class Utility:
+    """pomocne metody"""
+
+    @staticmethod
+    def result_by_hours(history: BaseManager[EmployeeStatusHistory]) -> int:
+        """vysledek preveden na hodiny"""
+        time_spent = 0
+        for record in history:
+            previous_record = history.filter(
+                timestamp__lt=record.timestamp
+            ).last()
+            if previous_record:
+                time_spent += (
+                    record.timestamp - previous_record.timestamp
+                ).seconds // 3600  # V hodinách
+
+        return time_spent
+
+
 if __name__ == "__main__":
-    pass
+    ...
